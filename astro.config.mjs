2435c26443d5e6bf68c89d5ff5986016ace0bf1f
Aidan Kinzett <aidan@labrys.io>
2023-02-04 23:01:15 +1000

    add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,18 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
   markdown: {
-    rehypePlugins: [
-      "rehype-slug",
-      "rehype-external-links",
-      [
-        "rehype-toc",
-        {
-          headings: ["h1", "h2"],
-          cssClasses: {
-            toc: "table-of-contents",
-          },
-        },
-      ],
-    ],
+    rehypePlugins: ["rehype-slug", "rehype-external-links", ["rehype-toc", {
+      headings: ["h1", "h2"],
+      cssClasses: {
+        toc: "table-of-contents"
+      }
+    }]]
   },
   site: "https://aidankinzett.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), compress()]
 });
