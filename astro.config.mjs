78f7d1dfcbd152027eb9c7a12ce2aec60c51f7c8
Aidan Kinzett <aidan@labrys.io>
2023-02-04 21:48:12 +1000

    add table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,25 @@
-import mdx from '@astrojs/mdx';
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro.aidankinzett.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  markdown: {
+    rehypePlugins: [
+      "rehype-slug",
+      "rehype-external-links",
+      [
+        "rehype-toc",
+        {
+          headings: ["h1", "h2"],
+          cssClasses: {
+            toc: "table-of-contents",
+          },
+        },
+      ],
+    ],
+  },
+  site: "https://astro.aidankinzett.com",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
