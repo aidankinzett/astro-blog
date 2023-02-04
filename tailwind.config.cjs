36a0a525a53bf1a51ff6403138781edca9ee940a
Aidan Kinzett <aidan@labrys.io>
2023-02-04 17:08:16 +1000

    improve header more

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      celeste: "#c5fffdff",
+      "middle-blue": "#88d9e6ff",
+      "cool-grey": "#8b8baeff",
+      feldgrau: "#526760ff",
+      "dark-slate-gray": "#374b4aff",
+    },
+  },
+  plugins: [],
+};
