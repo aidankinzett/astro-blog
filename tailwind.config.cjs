6b61eacf147bcf83701dd4f008c9671812a0edd9
Aidan Kinzett <aidan@labrys.io>
2023-02-04 20:40:28 +1000

    change colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = {
+  "blue-chill": {
+    50: "#ecfdff",
+    100: "#d0f7fd",
+    200: "#a7edfa",
+    300: "#6bdef5",
+    400: "#27c6e9",
+    500: "#0ba8cf",
+    600: "#0d8db6",
+    700: "#116c8d",
+    800: "#175973",
+    900: "#184961",
+  },
+};
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        celeste: "#c5fffdff",
-        "middle-blue": "#88d9e6ff",
-        "cool-grey": "#8b8baeff",
-        feldgrau: "#526760ff",
-        "dark-slate-gray": "#374b4aff",
+        primary: colors["blue-chill"],
       },
     },
   },
