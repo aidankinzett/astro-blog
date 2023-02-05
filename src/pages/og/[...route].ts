fe00e20b83a42c63f9874fbd8a3a7869b86f8ee7
Aidan Kinzett <aidan@labrys.io>
2023-02-05 22:17:37 +1000

    change where og images are outputter

--- src/pages/og/[...route].ts
+++ src/pages/og/[...route].ts
@@ -8,6 +8,15 @@ const config = resolveConfig(tailwindConfig);
 const hex = (config.theme?.colors as any)?.primary?.[200];
 const rgb = hexRgb(hex);
 
+// Import all pages from the content directory
+const pages = import.meta.glob("/src/content/**/*.md", { eager: true });
+
+// Remove the /src/content prefix from the paths
+const newPages = Object.entries(pages).reduce((acc, [path, page]) => {
+  const newPath = path.replace("/src/content", "");
+  return { ...acc, [newPath]: page };
+}, {});
+
 export const { getStaticPaths, get } = OGImageRoute({
   // Tell us the name of your dynamic route segment.
   // In this case it’s `route`, because the file is named `[...route].ts`.
@@ -15,7 +24,7 @@ export const { getStaticPaths, get } = OGImageRoute({
 
   // A collection of pages to generate images for.
   // This can be any map of paths to data, not necessarily a glob result.
-  pages: import.meta.glob("/src/content/**/*.md", { eager: true }),
+  pages: newPages,
 
   // For each page, this callback will be used to customize the OpenGraph
   // image. For example, if `pages` was passed a glob like above, you
@@ -38,6 +47,5 @@ export const { getStaticPaths, get } = OGImageRoute({
     },
     fonts: ["https://aidankinzett.com/fonts/BerkeleyMonoVariable-Regular.ttf"],
     bgGradient: [[rgb.red, rgb.green, rgb.blue]],
-   
   }),
 });
