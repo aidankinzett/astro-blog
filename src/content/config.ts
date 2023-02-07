f9f4a6b93c46c2be1b04c826e9c6459ce4c94653
Aidan Kinzett <aidan@labrys.io>
2023-02-07 22:43:38 +1000

    add TIL section

--- src/content/config.ts
+++ src/content/config.ts
@@ -21,4 +21,25 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const til = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    toc: z.boolean().default(false).optional(),
+    draft: z.boolean().default(true).optional(),
+    // --- Generated ---
+    ogImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, til };
---
+++ src/content/til/hello-world.md
@@ -0,0 +1,12 @@
+---
+title: "Hello World"
+description: "Hi, this is my first post..."
+pubDate: "Feb 5 2023"
+draft: false
+---
+
+# Hello
+
+Hi, this is my first post.
+
+For an introduction see my [About page](/about).
